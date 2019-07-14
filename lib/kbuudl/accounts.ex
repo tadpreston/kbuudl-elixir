8cf77c32c10cdddb6a510b542465aff904e276d8
Tad Preston <tadpreston@gmail.com>
2019-07-14 16:32:18 -0500

    lots of stuff was done

--- lib/kbuudl/accounts.ex
+++ lib/kbuudl/accounts.ex
@@ -5,8 +5,8 @@ defmodule Kbuudl.Accounts do
 
   import Ecto.Query, warn: false
   alias Kbuudl.Repo
-
   alias Kbuudl.Accounts.User
+  alias Kbuudl.Accounts.Credential
 
   @doc """
   Returns the list of users.
@@ -100,5 +100,91 @@ defmodule Kbuudl.Accounts do
   """
   def change_user(%User{} = user) do
     User.changeset(user, %{})
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Returns the list of credentials.
+
+  ## Examples
+
+      iex> list_credentials()
+      [%Credential{}, ...]
+
+  """
+  def list_credentials do
+    Repo.all(Credential)
+  end
+
+  @doc """
+  Gets a single credential.
+
+  Raises `Ecto.NoResultsError` if the Credential does not exist.
+
+  ## Examples
+
+      iex> get_credential!(123)
+      %Credential{}
+
+      iex> get_credential!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_credential!(id), do: Repo.get!(Credential, id)
+
+  @doc """
+  Updates a credential.
+
+  ## Examples
+
+      iex> update_credential(credential, %{field: new_value})
+      {:ok, %Credential{}}
+
+      iex> update_credential(credential, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_credential(%Credential{} = credential, attrs) do
+    credential
+    |> Credential.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Credential.
+
+  ## Examples
+
+      iex> delete_credential(credential)
+      {:ok, %Credential{}}
+
+      iex> delete_credential(credential)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_credential(%Credential{} = credential) do
+    Repo.delete(credential)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking credential changes.
+
+  ## Examples
+
+      iex> change_credential(credential)
+      %Ecto.Changeset{source: %Credential{}}
+
+  """
+  def change_credential(%Credential{} = credential) do
+    Credential.changeset(credential, %{})
   end
 end
--- test/kbuudl/accounts_test.exs
+++ test/kbuudl/accounts_test.exs
@@ -7,7 +7,7 @@ defmodule Kbuudl.AccountsTest do
     alias Kbuudl.Accounts.User
 
     @valid_attrs %{name: "some name", username: "some username"}
-    @update_attrs %{name: "some updated name", username: "some updated username"}
+    @update_attrs %{name: "some updated name", username: "some updated usrnm"}
     @invalid_attrs %{name: nil, username: nil}
 
     def user_fixture(attrs \\ %{}) do
@@ -43,7 +43,7 @@ defmodule Kbuudl.AccountsTest do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
       assert user.name == "some updated name"
-      assert user.username == "some updated username"
+      assert user.username == "some updated usrnm"
     end
 
     test "update_user/2 with invalid data returns error changeset" do
@@ -61,6 +61,57 @@ defmodule Kbuudl.AccountsTest do
     test "change_user/1 returns a user changeset" do
       user = user_fixture()
       assert %Ecto.Changeset{} = Accounts.change_user(user)
+    end
+  end
+
+  describe "credentials" do
+    alias Kbuudl.Accounts.Credential
+    alias Kbuudl.Accounts.User
+
+    @valid_attrs %{name: "some name", username: "some username", credential: %{email: "some email", password: "some password_hash"}}
+    @update_attrs %{email: "some updated email", password: "some updated password_hash"}
+    @invalid_attrs %{email: nil, password: nil}
+
+    def credential_fixture(attrs \\ %{}) do
+      {:ok, %User{credential: credential}} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Accounts.register_user()
+
+      credential
+    end
+
+    test "list_credentials/0 returns all credentials" do
+      credential = credential_fixture()
+      assert Accounts.list_credentials() == [credential]
+    end
+
+    test "get_credential!/1 returns the credential with given id" do
+      credential = credential_fixture()
+      assert Accounts.get_credential!(credential.id) == credential
+    end
+
+    test "update_credential/2 with valid data updates the credential" do
+      credential = credential_fixture()
+      assert {:ok, %Credential{} = credential} = Accounts.update_credential(credential, @update_attrs)
+      assert credential.email == "some updated email"
+    end
+
+    test "update_credential/2 with invalid data returns error changeset" do
+      credential = credential_fixture()
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_credential(credential, @invalid_attrs)
+      assert credential == Accounts.get_credential!(credential.id)
+    end
+
+    test "delete_credential/1 deletes the credential" do
+      credential = credential_fixture()
+      assert {:ok, %Credential{}} = Accounts.delete_credential(credential)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_credential!(credential.id) end
+    end
+
+    test "change_credential/1 returns a credential changeset" do
+      credential = credential_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_credential(credential)
     end
   end
 end
