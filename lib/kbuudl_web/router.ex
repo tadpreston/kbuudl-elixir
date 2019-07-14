dd33fe6b694f2a1a3d8d972d46ecb9e93e6350f9
Tad Preston <tadpreston@gmail.com>
2019-07-13 20:14:25 -0500

    Limit routes to new and create

--- lib/kbuudl_web/router.ex
+++ lib/kbuudl_web/router.ex
@@ -17,7 +17,7 @@ defmodule KbuudlWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    resources "/users", UserController
+    resources "/users", UserController, only: [:new, :create]
   end
 
   # Other scopes may use custom stacks.
