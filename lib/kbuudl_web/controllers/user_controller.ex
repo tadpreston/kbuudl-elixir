2430534511e5796d57e5af4a4d6857ff32fbcc38
Tad Preston <tadpreston@gmail.com>
2019-07-14 17:10:26 -0500

    Added the email and password the the user form

--- lib/kbuudl_web/controllers/user_controller.ex
+++ lib/kbuudl_web/controllers/user_controller.ex
@@ -5,15 +5,15 @@ defmodule KbuudlWeb.UserController do
   alias Kbuudl.Accounts.User
 
   def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+    changeset = Accounts.change_registration(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
-      {:ok, _user} ->
+    case Accounts.register_user(user_params) do
+      {:ok, user} ->
         conn
-        |> put_flash(:info, "User created successfully.")
+        |> put_flash(:info, "#{user.name} created.")
         |> redirect(to: Routes.page_path(conn, :index))
 
       {:error, %Ecto.Changeset{} = changeset} ->
